5afc9be009d3066a0788a4bc7ac23cca6b9bad83
Benjamin Kelley <mwpuppire@gmail.com>
2023-08-01 11:10:15 -0400

    doxygen add ht-hash functions as memberof ht_hash_table

--- ht-hash.h
+++ ht-hash.h
@@ -17,8 +17,9 @@ struct _ht_item;
  * \struct ht_hash_table
  * \brief The hash table class.
  *
- * The hash table class. Should be zero-initialized. `_ht_item` is intentionally
- * kept opaque; access to stored values should only be through `ht` functions.
+ * A hash table for string keys and values. Should be zero-initialized.
+ * `_ht_item` is intentionally kept opaque; access to stored values should only
+ * be through `ht` functions.
  * `ht_clear` should be called before a table exits scope if any values were
  * inserted into it to properly release memory.
  */
@@ -50,6 +51,7 @@ __attribute__((nonnull(1), nothrow))
  * before letting a hash table leave scope, if any values were added to the
  * table.
  *
+ * \memberof ht_hash_table
  * \param ht The hash table to clear
  */
 void ht_clear(ht_hash_table *ht);
@@ -62,6 +64,7 @@ __attribute__((nonnull(1, 2), nothrow, pure))
  *
  * Returns `true` if `key` exists in `ht`, and `false` otherwise.
  *
+ * \memberof ht_hash_table
  * \param ht The table to search in
  * \param key The key to search for
  * \param key_len The length of `key`, in bytes
@@ -77,6 +80,7 @@ __attribute__((nonnull(1, 2), nothrow, pure))
  * Returns `true` if `key` exists in `ht`, and `false` otherwise. Key length is
  * determined via the `strlen` function.
  *
+ * \memberof ht_hash_table
  * \param ht The table to search in
  * \param key The key to search for
  */
@@ -92,6 +96,7 @@ __attribute__((nonnull(1), nothrow))
  * items. `resize` never lowers capacity of a hash-table, though; if you want to
  * reduce memory usage, use `ht_shrink_to_fit` instead.
  *
+ * \memberof ht_hash_table
  * \param ht The table to resize
  * \param min_size The minimum number of items `ht` should be able to hold
  */
@@ -107,6 +112,7 @@ __attribute__((nonnull(1), nothrow))
  * Note that, as currently implemented, capacity has to be a power of two, so
  * capacity may still end up significantly larger than the number of items.
  *
+ * \memberof ht_hash_table
  * \param ht The table to resize
  */
 void ht_shrink_to_fit(ht_hash_table *ht);
@@ -122,6 +128,7 @@ __attribute__((nonnull(1, 2, 4), nothrow))
  * associated with `key` with the new value. Returns `true` if `key` was added
  * successfully, and `false` on error.
  *
+ * \memberof ht_hash_table
  * \param ht The table to insert into
  * \param key The key to insert under
  * \param key_len The length of `key`, in bytes
@@ -142,6 +149,7 @@ __attribute__((nonnull(1, 2, 4), nothrow))
  * keys, but behavior is undefined if `key` is contained within `ht` before
  * calling. Returns `true` on success and `false` on error.
  *
+ * \memberof ht_hash_table
  * \param ht The table to insert into
  * \param key The key to insert under
  * \param key_len The length of `key`, in bytes
@@ -161,6 +169,7 @@ __attribute__((nonnull(1, 2, 3), nothrow))
  * successfully, and `false` on error. Key and value length are determined via
  * the `strlen` function.
  *
+ * \memberof ht_hash_table
  * \param ht The table to insert into
  * \param key The key to insert under
  * \param value The value to insert under `key`
@@ -180,6 +189,7 @@ __attribute__((nonnull(1, 2, 3), nothrow))
  * calling. Returns `true` on success and `false` on error. Key and value length
  * are determined via the `strlen` function.
  *
+ * \memberof ht_hash_table
  * \param ht The table to insert into
  * \param key The key to insert under
  * \param value The value to insert under `key`
@@ -195,6 +205,7 @@ __attribute__((nonnull(1, 2), nothrow, pure))
  * Looks up `key` in `ht` and returns the associated value, or `NULL` if the key
  * doesn't exist in `ht`.
  *
+ * \memberof ht_hash_table
  * \param ht The table to search through
  * \param key The key to search for
  * \param key_len The length of `key`, in bytes
@@ -210,6 +221,7 @@ __attribute__((nonnull(1, 2), nothrow, pure))
  * Looks up `key` in `ht` and returns the associated value, or `NULL` if the key
  * doesn't exist in `ht`. Key length is determined via the `strlen` function.
  *
+ * \memberof ht_hash_table
  * \param ht The table to search through
  * \param key The key to search for
  */
@@ -226,6 +238,7 @@ __attribute__((nonnull(1, 2), nothrow))
  * `true` if `key` was removed from `ht` and `false` if `key` didn't exist in
  * `ht`. `ht` is unmodified if the key didn't exist in it.
  *
+ * \memberof ht_hash_table
  * \param ht The table to remove from
  * \param key The key to remove
  * \param key_len The length of `key`, in bytes
@@ -243,6 +256,7 @@ __attribute__((nonnull(1, 2), nothrow, warn_unused_result))
  * The caller is made responsible for freeing the returned value. This returns
  * `NULL` and has no effect on `ht` if `key` doesn't exist in `ht`.
  *
+ * \memberof ht_hash_table
  * \param ht The table to remove from
  * \param key The key to remove
  * \param key_len The length of `key`, in bytes
@@ -261,6 +275,7 @@ __attribute__((nonnull(1, 2), nothrow))
  * `ht`. `ht` is unmodified if the key didn't exist in it. Key length is
  * determined via the `strlen` function.
  *
+ * \memberof ht_hash_table
  * \param ht The table to remove from
  * \param key The key to remove
  */
@@ -277,6 +292,7 @@ __attribute__((nonnull(1, 2), nothrow, warn_unused_result))
  * `NULL` and has no effect on `ht` if `key` doesn't exist in `ht`. Key length
  * is determined via the `strlen` function.
  *
+ * \memberof ht_hash_table
  * \param ht The table to remove from
  * \param key The key to remove
  */
@@ -295,6 +311,7 @@ __attribute__((nonnull(1), nothrow, pure))
  * both `ht_insert` functions. `const` operations are safe to use, as well as
  * (in the current implementation) `ht_remove`.
  *
+ * \memberof ht_hash_table
  * \param ht The table to iterate over
  */
 ht_iter ht_iterator(const ht_hash_table *ht);
@@ -308,6 +325,7 @@ __attribute__((nonnull(1), nothrow))
  * Advances `iter` and returns the next key, or `NULL` if the iterator was
  * empty.
  *
+ * \memberof ht_hash_table
  * \param iter The iterator to advance
  */
 char *ht_iter_next(ht_iter *iter);
@@ -323,6 +341,7 @@ __attribute__((nonnull(1), nothrow))
  * set to the key and value of the next pair, respectively; if there was no next
  * pair, `key` and `val` are unchanged.
  *
+ * \memberof ht_hash_table
  * \param iter The iterator to advance
  * \param key If non-`NULL`, is set to the pair's key
  * \param val If non-`NULL`, is set to the pair's value
@@ -348,6 +367,7 @@ __attribute__((nonnull(1), nothrow))
  * values may not hold valid JSON strings, use `ht_json_stringify_escape`
  * instead.
  *
+ * \memberof ht_hash_table
  * \param ht The table to stringify
  * \param out Where to place the created buffer
  */
@@ -371,6 +391,7 @@ __attribute__((nonnull(1), nothrow))
  * slower even without any quotes to escape, so if you know there won't be
  * quotes in keys or values, `ht_json_stringify` should be used.
  *
+ * \memberof ht_hash_table
  * \param ht The table to stringify
  * \param out Where to place the created buffer
  */
